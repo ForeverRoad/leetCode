e9064024ab8fb276ca5b689c95100f77df8b152e
zcf <237512382@qq.com>
2019-01-09 22:33:36 +0800

    toLowerCase

---
+++ src/main/java/easy/ToLowerCase.java
@@ -0,0 +1,26 @@
+package easy;
+
+/**
+ * @Auther zcf
+ * @Date 2019-01-09
+ * Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+ */
+public class ToLowerCase {
+    public static String toLowerCase(String str) {
+//        return str.toLowerCase();
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c >= 'A' && c <= 'Z') {
+                chars[i] = (char) (c - 'A' + 'a');
+            }
+        }
+        return new String(chars);
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("SdESdfde"));
+    }
+}
