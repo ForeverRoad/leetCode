41426a080e233b12df1c262b1c16ca04f18a10d2
zcf <237512382@qq.com>
2019-02-11 11:37:12 +0800

    fix

---
+++ src/main/java/easy/HammingDistance.java
@@ -0,0 +1,32 @@
+package easy;
+
+/**
+ * @Description TODO
+ * @Author zcf
+ * @Date 2019-02-11 11:08
+ **/
+public class HammingDistance {
+
+    public static void main(String[] args) {
+        System.out.println(hammingDistance(1, 4));
+    }
+
+    public static int hammingDistance(int x, int y) {
+        int result = 0;
+        int input = x ^ y;
+        //移位，把int转成二进制
+        //for (int i=0;i<32;i++) {
+        //  result += (input >>> i) & 1;
+        //}
+
+        //api  Integer.bitCount(i);
+        String str = Integer.toBinaryString(input);
+        char[] chars = str.toCharArray();
+        for (char c : chars) {
+            if (c == '1') {
+                result++;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/easy/RobotReturnToOrigin.java
@@ -0,0 +1,42 @@
+package easy;
+
+/**
+ * @Description There is a robot starting at position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
+ *
+ * The move sequence is represented by a string, and the character moves[i] represents its ith move. Valid moves are R (right), L (left), U (up), and D (down). If the robot returns to the origin after it finishes all of its moves, return true. Otherwise, return false.
+ *
+ * Note: The way that the robot is "facing" is irrelevant. "R" will always make the robot move to the right once, "L" will always make it move left, etc. Also, assume that the magnitude of the robot's movement is the same for each move.
+ *
+ * Example 1:
+ *
+ * Input: "UD"
+ * Output: true
+ * @Author zcf
+ * @Date 2019-02-11 10:35
+ **/
+public class RobotReturnToOrigin {
+
+    public static void main(String[] args) {
+        String moves = "LR";
+        System.out.println(judgeCircle(moves));
+    }
+
+    public static boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+        char[] chars = moves.toCharArray();
+        for (char c : chars) {
+            if (c == 'L') {
+                x--;
+            } else if (c == 'R') {
+                x++;
+            } else if (c == 'U') {
+                y++;
+            } else if (c == 'D') {
+                y--;
+            }
+        }
+        return x == y && x == 0;
+    }
+
+}
